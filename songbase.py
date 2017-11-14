5980ddfead1455fdbad28aac72e2735f97e71770
Wang Peiyu <oliviapy@udel.edu>
2017-11-14 13:41:55 -0500

    added web forms

--- songbase.py
+++ songbase.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 app = Flask(__name__)
+app.config['SECRET_KEY']='hgkjhkjhkjlhjkjk'
 
 @app.route('/')
 def home():##view function, the name needs to be unique
@@ -24,3 +25,16 @@ def get_all_songs():
 
 if __name__ == '__main__':
    app.run()##run it with a debug mode on
+
+@app.route('/form-demo', methods=['GET','POST'])
+def form_demo():##view function, the name needs to be unique
+    if request.method=='GET':
+        first_name=request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html',first_name=first_name)
+        else:
+           first_name=session.get('first_name')
+           return render_template('form-demo.html',first_name=first_name)
+    if request.method=='POST':
+        session['first_name']=request.form['first_name']
+        return redirect(url_for('form_demo'))
