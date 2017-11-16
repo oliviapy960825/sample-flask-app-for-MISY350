5768d3b37155bdda70742e68bf52b32e55121e85
Wang Peiyu <oliviapy@udel.edu>
2017-11-16 13:39:58 -0500

    added

--- manage.py
+++ manage.py
@@ -1,9 +1,22 @@
 from flask_script import Manager
-from songbase import app
-
+from songbase import app, db, Artist, Song
 
 manager=Manager(app)##create manager that manages the app created in songbase.py
 
+# reset the database and create two artists
+@manager.command
+def deploy():
+    db.drop_all()##make sure clean up all the DB
+    db.create_all()##create DB new and any table if defined
+    ##codes above are used to initialize the DB
+    ##below is optional
+    coldplay = Artist(name='Coldplay', about='Coldplay is a British rock band.')
+    maroon5 = Artist(name='Maroon 5', about='Maroon 5 is an American pop rock band.')
+    song1=Song(name='yellow', year=2004, lyrics='blah blah', artist=coldplay)##id is always automatically maintained
+    db.session.add(coldplay)
+    db.session.add(maroon5)
+    db.session.add(song1)
+    db.session.commit()##the 2 records are only added after you commit
 
 if __name__ == '__main__':
     manager.run()
--- songbase.py
+++ songbase.py
@@ -1,11 +1,41 @@
+import os ##get the current paths so the db can be stored locally
 from flask import Flask, render_template, request, flash, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY']='hgkjhkjhkjlhjkjk'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+# define database tables
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    about = db.Column(db.Text)
+    songs = db.relationship('Song', backref='artist')##back reference at the many side, unique in ORM
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    year = db.Column(db.Integer)
+    lyrics = db.Column(db.Text)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+
 
 @app.route('/')
 def home():##view function, the name needs to be unique
     #return '<h1>hello world!!!!lllll</h1>'##codes have changed but the server hasn't been restarted
     return render_template('index.html')
+
+@app.route('/artists')
+def show_all_artists():
+    artists = Artist.query.all()
+    return render_template('artist-all.html', artists=artists)
 
 @app.route('/user/<string:name>/')
 def get_user(name):
@@ -13,14 +43,26 @@ def get_user(name):
     return render_template('user.html', user_name=name)
 
 
-@app.route('/songs')##every route needs a corresponding view function
-def get_all_songs():
-    songs=[
-    'song 1',
-    'song 2',
-    'song 3'
-    ]
-    return render_template('songs.html', songs=songs)
+# song-all.html adds song id to the edit button using a hidden input
+@app.route('/songs')
+def show_all_songs():
+    songs = Song.query.all()
+    return render_template('song-all.html', songs=songs)
+
+@app.route('/artist/add', methods=['GET', 'POST'])
+def add_artists():
+    if request.method == 'GET':
+        return render_template('artist-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        about = request.form['about']
+
+        # insert the data into the database
+        artist = Artist(name=name, about=about)
+        db.session.add(artist)
+        db.session.commit()
+        return redirect(url_for('show_all_artists'))
 
 
 if __name__ == '__main__':
